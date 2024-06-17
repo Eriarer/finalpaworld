71f0a8ff6b04cd979edd2def0d1d558413bd8b3f
eriarer <eriarer_messiah+git@hotmail.com>
2024-06-17 14:05:11 -0600

    chore: Add HttpClient provider to app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,10 +6,12 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment.development';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideHttpClient(),
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
