47b5fe831d2159d698485183eda86affa57600ab
eriarer <eriarer_messiah+git@hotmail.com>
2024-06-17 13:59:08 -0600

    Base components

---
+++ src/app/components/nav-user-state/nav-user-state.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-user-state',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-user-state.component.html',
+  styleUrl: './nav-user-state.component.css'
+})
+export class NavUserStateComponent {
+
+}
