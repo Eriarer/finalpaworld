b79e9f8ef94d18f2c1ce99c9e9b87f002f40f43f
eriarer <eriarer_messiah+git@hotmail.com>
2024-06-17 21:32:21 -0600

    feat: Update routes for new components and paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,27 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CitasComponent } from './components/citas/citas.component';
+import { MascotasComponent } from './components/mascotas/mascotas.component';
+import { AgendarComponent } from './components/agendar/agendar.component';
+import { GraficasComponent } from './components/graficas/graficas.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ConsultasComponent } from './components/consultas/consultas.component';
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'inicio', component: HomeComponent },
+  { path: 'mascotas', component: MascotasComponent },
+  { path: 'acercade', component: AcercaDeComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: 'agendar', component: AgendarComponent },
+  { path: 'agendar/:id', component: AgendarComponent },
+  { path: 'citas', component: CitasComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegisterComponent },
+  { path: 'consultas', component: ConsultasComponent },
+  { path: 'graficas', component: GraficasComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' },
 ];
