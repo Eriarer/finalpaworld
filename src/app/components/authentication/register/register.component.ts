043dac3489a5474d67e597eb3d08278e57c7b660
eriarer <eriarer_messiah+git@hotmail.com>
2024-06-22 12:57:50 -0600

    base module

--- src/app/components/authentication/register/register.component.ts
+++ src/app/components/authentication/register/register.component.ts
@@ -1,30 +1,108 @@
 import { Component } from '@angular/core';
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  PasswordStrengthValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const password = control.value;
-      if (password) {
-        const hasNumber = /[0-9]/.test(password);
-        const hasUpper = /[A-Z]/.test(password);
-        const hasLower = /[a-z]/.test(password);
-        const hasSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(
-          password
-        );
-        const valid = hasNumber && hasUpper && hasLower && hasSpecial;
-        if (!valid) {
-          return { passwordStrength: true };
-        }
-      }
-      return null;
-    };
+  name = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ],
+  });
+  nickname = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required],
+  });
+  email = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.email],
+  });
+  phoneExtension = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.pattern('^[0-9]*$')],
+  });
+  phoneNumber = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.pattern('^[0-9]*$')],
+  });
+  password = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(6),
+      this.passwordStrengthValidator,
+      this.passwordMatchValidator,
+    ],
+  });
+  confirmPassword = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      this.passwordStrengthValidator,
+      this.passwordMatchValidator,
+    ],
+  });
+
+  registerForm = new FormGroup({
+    name: this.name,
+    nickname: this.nickname,
+    email: this.email,
+    phoneExtension: this.phoneExtension,
+    phoneNumber: this.phoneNumber,
+    password: this.password,
+    confirmPassword: this.confirmPassword,
+  });
+
+  passwordStrengthValidator(control: AbstractControl) {
+    const password: string = control.value;
+    if (!password) return null;
+
+    const hasNumber = /[0-9]/.test(password);
+    const hasUpper = /[A-Z]/.test(password);
+    const hasLower = /[a-z]/.test(password);
+    const hasSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(password);
+
+    const valid = hasNumber && hasUpper && hasLower && hasSpecial;
+
+    return valid ? null : { passwordStrength: true };
+  }
+
+  passwordMatchValidator(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if (!password || !confirmPassword) return null;
+
+    return password === confirmPassword ? null : { passwordMatch: true };
+  }
+
+  onSubmitRegister() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    }
+    if (this.registerForm.invalid) {
+      console.log('Form is invalid');
+    }
+  }
+
+  showErrors() {
+    console.log(this.registerForm.get('password')!.errors);
   }
 }
