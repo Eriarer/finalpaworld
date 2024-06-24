b40aa8e8351861c04c8527ed1484cd7b74d74bc5
eriarer <eriarer_messiah+git@hotmail.com>
2024-06-24 17:07:46 -0600

    chore: Update login component styling and dependencies Co-authored-by: Mariana Ávila Rivera <mariana-avila-rivera@users.noreply.github.com> Co-authored-by: Leonardo <Laxlev@users.noreply.github.com>

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -1,23 +1,57 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
-
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatError } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatError,
+    CommonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
-
-  ngOnInit() {
-    // Si la ruta no es login/email o login/phone, redirigir a login/email
+  loginType: string = 'email';
+  constructor(private router: Router) {
     if (
       this.router.url !== '/login/email' &&
       this.router.url !== '/login/phone'
     ) {
       this.router.navigate(['/login/email']);
     }
+  }
+
+  ngOnInit() {
+    //escuchar los eventos de cambio de ruta
+    this.router.events.subscribe(() => {
+      if (
+        this.router.url !== '/login/email' &&
+        this.router.url !== '/login/phone'
+      ) {
+        this.router.navigate(['/login/email']);
+      }
+      this.loginType = this.router.url.split('/')[2];
+    });
+    // Si la ruta no es login/email o login/phone, redirigir a login/email
   }
 }
