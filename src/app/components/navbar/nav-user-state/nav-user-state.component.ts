9a0b0b1f362a401bfcec2cec350a8e80f412a144
eriarer <eriarer_messiah+git@hotmail.com>
2024-06-24 17:08:38 -0600

    chore: Update navbar component HTML formatting
    Co-authored-by: Mariana Ávila Rivera <mariana-avila-rivera@users.noreply.github.com>
    Co-authored-by: Leonardo <Laxlev@users.noreply.github.com>

--- src/app/components/navbar/nav-user-state/nav-user-state.component.ts
+++ src/app/components/navbar/nav-user-state/nav-user-state.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../services/firebase/auth.service';
+import { CommonModule } from '@angular/common';
+import { UsersFbService } from '../../../services/firebase/users-fb.service';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-nav-user-state',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, MatIcon],
   templateUrl: './nav-user-state.component.html',
   styleUrl: './nav-user-state.component.css',
 })
 export class NavUserStateComponent {
-  [x: string]: any;
-  userLogged = true;
+  userLogged: boolean = true;
+  admin: boolean = false;
+  displayName: string = '';
   // bt breakpoints
   bs_sm: number = 576;
   bs_md: number = 768;
@@ -22,9 +27,32 @@ export class NavUserStateComponent {
   adminRoute: boolean = false;
   // crear un listener para el tamaño de la pantalla
   // y cambiar el valor de dropstart
-  constructor(private router: Router) {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private userService: UsersFbService
+  ) {
     this.handleResize();
     window.addEventListener('resize', this.handleResize);
+    this.authService.getCurrentUser().subscribe((user) => {
+      if (user) {
+        console.log('User is signed in', user);
+        console.log('UserID', user.uid);
+        console.log('UserEmail', user.email);
+        console.log('UserPhone', user.phoneNumber);
+        console.log('UserDisplayName', user.displayName);
+        this.userLogged = true;
+        this.displayName = user.displayName!;
+        this.userService.isUserAdmin(user.uid).then((isAdmin) => {
+          this.admin = isAdmin;
+        });
+      } else {
+        console.log('No user signed in');
+        this.userLogged = false;
+        this.admin = false;
+        this.displayName = '';
+      }
+    });
   }
 
   handleResize = () => {
@@ -49,5 +77,11 @@ export class NavUserStateComponent {
         }
       }
     });
+  }
+
+  logout() {
+    this.router.navigate(['/inicio']);
+    this.authService.signOut();
+    window.location.reload();
   }
 }
