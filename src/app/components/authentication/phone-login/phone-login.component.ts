577f328b1196c26f9397f33e1df0a5dcc3a60328
eriarer <eriarer_messiah+git@hotmail.com>
2024-06-24 17:08:11 -0600

    chore: Update phone login component styling and dependencies Co-authored-by: Mariana Ávila Rivera <mariana-avila-rivera@users.noreply.github.com> Co-authored-by: Leonardo <Laxlev@users.noreply.github.com>

--- src/app/components/authentication/phone-login/phone-login.component.ts
+++ src/app/components/authentication/phone-login/phone-login.component.ts
@@ -1,11 +1,89 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatError, MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../../services/firebase/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-phone-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatError,
+    CommonModule,
+  ],
   templateUrl: './phone-login.component.html',
   styleUrl: './phone-login.component.css',
 })
-export class PhoneLoginComponent {}
+export class PhoneLoginComponent {
+  email = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.email],
+  });
+
+  loginForm = new FormGroup({
+    email: this.email,
+  });
+
+  codigo = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.required, Validators.minLength(6)],
+  });
+
+  confirmForm = new FormGroup({
+    codigo: this.codigo,
+  });
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  async onSubmitLogin() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted');
+    }
+    if (this.loginForm.invalid) {
+      console.log('Form not submitted');
+      console.log('Email Errors', this.loginForm.get('email')!.errors);
+      console.log('Password Errors', this.loginForm.get('password')!.errors);
+    }
+    if (this.loginForm.valid) {
+      console.log('Form submitted, value:' + this.loginForm.value);
+      let captchaContainer = document.getElementById('sendCoideButton')!;
+      //autenticando con email enlazado al telefono, autenticando con telefono
+      await this.authService
+        .singInWithPhoneNumberByEmail(this.email.value, captchaContainer)
+        .then((response) => {});
+    }
+  }
+
+  getEmailErrorMessage() {
+    const emailControl = this.loginForm.get('email');
+    if (emailControl?.hasError('required')) {
+      return 'Email is required';
+    }
+    return emailControl?.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  onSubmitConfirm() {
+    if (this.confirmForm.invalid) {
+      console.log('Form not submitted');
+      console.log('Codigo Errors', this.confirmForm.get('codigo')!.errors);
+    }
+    console.log('Form submitted', this.codigo.value);
+  }
+}
