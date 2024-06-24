98f74e8f6ef86b1b0943ae28fa1d8a2ee81c03c1
eriarer <eriarer_messiah+git@hotmail.com>
2024-06-24 17:07:56 -0600

    chore: Update email login component styling and dependencies Co-authored-by: Mariana Ávila Rivera <mariana-avila-rivera@users.noreply.github.com> Co-authored-by: Leonardo <Laxlev@users.noreply.github.com>

--- src/app/components/authentication/email-login/email-login.component.ts
+++ src/app/components/authentication/email-login/email-login.component.ts
@@ -6,16 +6,34 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
+import { Router, RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatError } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../services/firebase/auth.service';
 @Component({
   selector: 'app-email-login',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, RouterModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    RouterModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatError,
+  ],
   templateUrl: './email-login.component.html',
   styleUrl: './email-login.component.css',
 })
 export class EmailLoginComponent {
+  hidePassword: boolean = true;
+
   password = new FormControl('', {
     nonNullable: true,
     validators: [Validators.required, Validators.minLength(6)],
@@ -31,14 +49,42 @@ export class EmailLoginComponent {
     password: this.password,
   });
 
+  constructor(private router: Router, private authService: AuthService) {}
+
   onSubmitLogin() {
-    if (this.loginForm.valid) {
-      console.log('Form submitted');
-    }
     if (this.loginForm.invalid) {
       console.log('Form not submitted');
       console.log('Email Errors', this.loginForm.get('email')!.errors);
       console.log('Password Errors', this.loginForm.get('password')!.errors);
+      return;
     }
+    console.log('Form submitted');
+    let loginButton = document.getElementById('loginButton')!;
+    this.authService
+      .signInWithEmailAndPassword(
+        this.email.value,
+        this.password.value,
+        loginButton
+      )
+      .then(() => {
+        this.router.navigate(['/inicio']);
+      });
+  }
+
+  getPasswordErrorMessage() {
+    const passwordControl = this.loginForm.get('password');
+    if (passwordControl?.hasError('required')) {
+      return 'Password is required';
+    }
+    return passwordControl?.hasError('minlength')
+      ? 'Password must be at least 6 characters long'
+      : '';
+  }
+  getEmailErrorMessage() {
+    const emailControl = this.loginForm.get('email');
+    if (emailControl?.hasError('required')) {
+      return 'Email is required';
+    }
+    return emailControl?.hasError('email') ? 'Not a valid email' : '';
   }
 }
