fd9ed60ded65d8d784a78da902c4094317fabfc2
AketzaliCastorena <aketzalicastorena18@gmail.com>
2024-06-25 17:09:55 -0600

    se agregaron modificaciones

--- src/app/components/admin/graficas/graficas.component.ts
+++ src/app/components/admin/graficas/graficas.component.ts
@@ -1,59 +1,87 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Chart, ChartType } from 'chart.js/auto';
-
+import { CitasFbService } from '../../../services/firebase/citas-fb.service';
+import { LoaderComponent } from '../../loader/loader.component';
 
 @Component({
   selector: 'app-graficas',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './graficas.component.html',
-  styleUrl: './graficas.component.css'
+  styleUrl: './graficas.component.css',
 })
-export class GraficasComponent implements OnInit{
-  
+export class GraficasComponent implements OnInit {
   public chart: Chart | null = null;
+  isLoading = false;
+  dias: string[] = [];
+  citas: number[] = [];
 
   private CHART_COLORS = {
     red: 'rgb(255, 99, 132)',
-    blue: 'rgb(54, 162, 235)'
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(90, 120, 255)',
   };
+
+  constructor(private citasService: CitasFbService) {}
+
   ngOnInit(): void {
-    this.initializeChart();
+    this.getData();
+  }
+
+  getData() {
+    this.isLoading = true;
+    this.citasService
+      .getUltimasSieteDias()
+      .then((data) => {
+        console.log('last seven days', data);
+        data.forEach((dia) => {
+          this.dias.push(dia.fecha);
+          this.citas.push(dia.cantCitas);
+        });
+        this.initializeChart();
+      })
+      .catch((error) => {
+        console.log('Ooops No se pudieron obtener los datos', error);
+      });
   }
 
   initializeChart(): void {
     const data = {
-      labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
-      datasets: [{
-        label: 'Dataset',
-        data: this.generateRandomNumbers(6, -100, 100),
-        borderColor: this.CHART_COLORS.red,
-        backgroundColor: this.transparentize(this.CHART_COLORS.red, 0.5),
-        pointStyle: 'circle',
-        pointRadius: 10,
-        pointHoverRadius: 15
-      }]
+      labels: this.dias,
+      datasets: [
+        {
+          label: 'Citas por dia',
+          data: this.citas,
+          borderColor: this.CHART_COLORS.purple,
+          backgroundColor: this.transparentize(this.CHART_COLORS.blue, 0.5),
+          pointStyle: 'circle',
+          pointRadius: 5,
+          pointHoverRadius: 15,
+        },
+      ],
     };
 
     const ctx = document.getElementById('chart') as HTMLCanvasElement;
     this.chart = new Chart(ctx, {
       type: 'line' as ChartType,
-      data: data
+      data: data,
+      options: {
+        plugins: {
+          legend: {
+            display: true,
+            position: 'bottom',
+            labels: {
+              color: 'black',
+            },
+          },
+        },
+      },
     });
-  }
-
-  private generateRandomNumbers(count: number, min: number, max: number, decimalPlaces: number = 2): number[] {
-    const numArray = [];
-    for (let i = 0; i < count; i++) {
-      const randomValue = (Math.random() * (max - min) + min).toFixed(decimalPlaces);
-      numArray.push(parseFloat(randomValue));
-    }
-    return numArray;
+    this.isLoading = false;
   }
 
   private transparentize(color: string, opacity: number): string {
     const alpha = 1 - opacity;
     return color.replace('rgb(', 'rgba(').replace(')', `, ${alpha})`);
   }
-  
 }
