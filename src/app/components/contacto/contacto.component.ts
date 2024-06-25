848bfded061cd063fa2247858230ca3da980b98d
diosescalera <al217108@edu.uaa.mx>
2024-06-24 20:15:07 -0600

    Add form validation and submission functionality to ContactoComponent

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,12 +1,63 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contacto',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './contacto.component.html',
-  styleUrl: './contacto.component.css'
+  styleUrl: './contacto.component.css',
 })
 export class ContactoComponent {
+  resultado!: string;
 
+  formularioContacto = new FormGroup({
+    nombre: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(50),
+    ]),
+    asunto: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(50),
+    ]),
+    correo: new FormControl('', [Validators.required, Validators.email]),
+    mensaje: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(500),
+    ]),
+  });
+
+  constructor(private http: HttpClient) {}
+
+  submit() {
+    if (this.formularioContacto.valid) {
+      console.log(this.formularioContacto.value);
+      const url = 'http://localhost:3000/correo';
+      this.http
+        .post('http://localhost:3000/contacto', this.formularioContacto.value)
+        .subscribe(
+          (res) => {
+            console.log(res);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+
+      this.resultado =
+        'Gracias por tus comentarios ' +
+        this.formularioContacto.value.nombre +
+        '.<br>Enviaremos una respuesta a tu correo ' +
+        this.formularioContacto.value.correo +
+        '.';
+    } else
+      this.resultado = 'Por favor, completa correctamente todos los campos.';
+  }
 }
