be02548ff4e9659c6ce447488fb661678fbae951
AketzaliCastorena <aketzalicastorena18@gmail.com>
2024-06-25 22:36:29 -0600

    se agregaron imports y modificaron algunas cosas

--- src/app/components/admin/consultas/consultas.component.ts
+++ src/app/components/admin/consultas/consultas.component.ts
@@ -1,12 +1,222 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import {
+  MatDatepicker,
+  MatDatepickerInputEvent,
+  MatDatepickerIntl,
+  MatDatepickerModule,
+  MatDatepickerToggle,
+} from '@angular/material/datepicker';
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+} from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CitasFbService } from '../../../services/firebase/citas-fb.service';
+import { CommonModule } from '@angular/common';
+import { Cita } from '../../../interfaces/cita';
+import { TimestampToDateStringPipe } from '../../../pipes/timestamp-to-date-string.pipe';
+import { TimestampToHourStringPipe } from '../../../pipes/timestamp-to-hour-string.pipe';
+import { TitleCasePipe, UpperCasePipe } from '@angular/common';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
+
+/////////////
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MascotasService } from '../../../services/data/mascotas.service';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { Mascota } from '../../../interfaces/mascota';
+import { Adoptante } from '../../../interfaces/adoptante';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Subscription, merge } from 'rxjs';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import 'moment/locale/fr';
+import Swal from 'sweetalert2';
+import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../../services/firebase/auth.service';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { LoaderComponent } from '../../loader/loader.component';
+import { UsersFbService } from '../../../services/firebase/users-fb.service';
+
+// Define custom date format for fr locale
+export const FR_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-consultas',
   standalone: true,
-  imports: [],
+  providers: [
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'fr' },
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_FORMATS, useValue: FR_DATE_FORMATS },
+  ],
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatHint,
+    MatDatepicker,
+    MatDatepickerToggle,
+    MatDatepickerModule,
+    MatIcon,
+    MatButton,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatError,
+    FormsModule,
+    CommonModule,
+    TimestampToDateStringPipe,
+    TimestampToHourStringPipe,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatDividerModule,
+    MatSelectModule,
+    RouterOutlet,
+    LoaderComponent,
+  ],
   templateUrl: './consultas.component.html',
-  styleUrl: './consultas.component.css'
+  styleUrl: './consultas.component.css',
 })
 export class ConsultasComponent {
+  isLoading = false;
 
+  //datos para el calendario
+  minDate!: Date;
+  maxDate!: Date;
+
+  //Para formatear la fecha (DD/MM/YYYY)
+  private _adapter!: DateAdapter<any>;
+  private _intl!: MatDatepickerIntl;
+  @Inject(MAT_DATE_LOCALE) private _locale!: string;
+
+  gatoBoxControl = new FormControl('', { nonNullable: true });
+  perroBoxControl = new FormControl('', { nonNullable: true });
+  citas: any[] = [];
+
+  nameControl = new FormControl('', {
+    nonNullable: true,
+    validators: [Validators.maxLength(50), Validators.pattern('^[a-zA-Z ]*$')],
+  });
+
+  fechaControl = new FormControl('', {
+    nonNullable: true,
+  });
+
+  multiQueryForm = new FormGroup({
+    gatoBoxControl: this.gatoBoxControl,
+    perroBoxControl: this.perroBoxControl,
+    nameControl: this.nameControl,
+    fechaControl: this.fechaControl,
+  });
+
+  constructor(private citasService: CitasFbService) {
+    this.isLoading = true;
+    this.citasService
+      .getAllCitas()
+      .then((citas: any) => {
+        this.citas = citas;
+      })
+      .catch((error) => {
+        this.isLoading = false;
+        Swal.fire('Error', 'Error al obtener las citas', 'error');
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+
+    let fechaActual = new Date();
+    //La fecha mínima es la fecha actual +1
+    this.minDate = new Date(
+      fechaActual.getFullYear(),
+      fechaActual.getMonth() - 2,
+      fechaActual.getDate()
+    );
+    //La fecha máxima es la fecha actual +2 meses
+    this.maxDate = new Date(
+      fechaActual.getFullYear(),
+      fechaActual.getMonth() + 2,
+      fechaActual.getDate()
+    );
+  }
+
+  onSubmitQuery() {
+    if (this.multiQueryForm.invalid) {
+      console.log('Formulario inválido');
+      return;
+    }
+    console.log('Formulario válido');
+    let date = new Date();
+    const perroVal = this.perroBoxControl.value ? true : false;
+    const gatoVal = this.gatoBoxControl.value ? true : false;
+    const fecha = new Date(this.fechaControl.value);
+    fecha.setHours(0, 0, 0, 0);
+    this.isLoading = true;
+    this.citasService
+      .getMultiQueryCitas(this.nameControl.value, fecha, perroVal, gatoVal)
+      .then((citas: any) => {
+        this.citas = citas;
+        console.log(this.citas);
+      })
+      .catch((error) => {
+        this.isLoading = false;
+        console.log('Error al obtener las citas');
+        Swal.fire('Error', 'Error al obtener las citas', 'error');
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  }
+
+  resetForm() {
+    this.multiQueryForm.reset();
+    this.isLoading = true;
+    this.citasService
+      .getAllCitas()
+      .then((citas: any) => {
+        this.citas = citas;
+      })
+      .catch((error) => {
+        this.isLoading = false;
+        console.log('Error al obtener las citas', error);
+        Swal.fire('Error', 'Error al obtener las citas', 'error');
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  }
 }
