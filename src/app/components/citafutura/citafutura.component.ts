bb1f01ca4676d6bd486b59998119cad65d6ac1b8
mariana-avila-rivera <marianaavilarivera@gmail.com>
2024-06-25 21:29:01 -0600

    Btn cancelar citas implementado y funcional, estilos en citas Co-authored-by: Leonardo <Laxlev@users.noreply.github.com>

--- src/app/components/citafutura/citafutura.component.ts
+++ src/app/components/citafutura/citafutura.component.ts
@@ -14,16 +14,25 @@ import {
 import { TimestampToDateStringPipe } from '../../pipes/timestamp-to-date-string.pipe';
 import { TimestampToHourStringPipe } from '../../pipes/timestamp-to-hour-string.pipe';
 
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from '../loader/loader.component';
+
 @Component({
   selector: 'app-citafutura',
   standalone: true,
-  imports: [TimestampToDateStringPipe, TimestampToHourStringPipe],
+  imports: [
+    TimestampToDateStringPipe,
+    TimestampToHourStringPipe,
+    CommonModule,
+    LoaderComponent,
+  ],
   templateUrl: './citafutura.component.html',
   styleUrl: './citafutura.component.css',
 })
 export class CitafuturaComponent {
   citas: any = [];
   fechaActual = new Date();
+  mostrar = 'block';
 
   constructor(
     private CitasFbService: CitasFbService,
@@ -33,11 +42,46 @@ export class CitafuturaComponent {
   }
 
   async cargacitasFuturas() {
-    //dateObject = new Date(fechaHora);
+    // Actualizando estilos
+    let style = document.getElementsByClassName('eliminar');
+    let btnCitasFuturas = document.getElementById('btnCitasF');
+    let btnCitasPasadas = document.getElementById('btnCitasP');
+    if (btnCitasFuturas && btnCitasPasadas) {
+      btnCitasFuturas.setAttribute(
+        'style',
+        'font-weight: bold; color: #04125a; border-bottom: 2px solid  #ff4081;'
+      );
+      btnCitasPasadas.setAttribute(
+        'style',
+        'font-weight: none; color: #04125a; border-bottom: 2px solid transparent;'
+      );
+    }
+    for (let i = 0; i < style.length; i++) {
+      style[i].setAttribute('style', 'display: table-cell');
+    }
+    //Se obtienen las citas futuras
     this.citas = await this.CitasFbService.getCitasFuturas(this.fechaActual);
   }
 
   async cargacitasPrevias() {
+    let style = document.getElementsByClassName('eliminar');
+    //recorremos los objetos con clase eliminar
+    let btnCitasPasadas = document.getElementById('btnCitasP');
+    let btnCitasFuturas = document.getElementById('btnCitasF');
+    if (btnCitasFuturas && btnCitasPasadas) {
+      btnCitasPasadas.setAttribute(
+        'style',
+        'font-weight: bold; color: #04125a; border-bottom: 2px solid  #ff4081;'
+      );
+      btnCitasFuturas.setAttribute(
+        'style',
+        'font-weight: none; color: #04125a; border-bottom: 2px solid transparent;'
+      );
+    }
+    for (let i = 0; i < style.length; i++) {
+      //ocultamos los objetos con clase eliminar
+      style[i].setAttribute('style', 'display: none');
+    }
     this.citas = await this.CitasFbService.getCitasPasadas(this.fechaActual);
   }
 
@@ -47,5 +91,16 @@ export class CitafuturaComponent {
 
   btnCargarFuturas(): void {
     this.cargacitasFuturas();
+  }
+
+  async btnEliminarCita(cita: string) {
+    console.log(cita);
+    if (!cita) {
+      return;
+    } else {
+      console.log('Eliminando cita' + cita);
+      await this.CitasFbService.deleteCita(cita);
+      this.cargacitasFuturas();
+    }
   }
 }
