13bedc745880ffae0d8b85d8a910252383df7cbc
eriarer <eriarer_messiah+git@hotmail.com>
2024-06-26 17:46:13 -0600

    refactor: Update QRData service to use HTTPS for API calls

--- src/app/services/qr/qrdata.service.ts
+++ src/app/services/qr/qrdata.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { firstValueFrom } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QrdataService {
-  baseUrl: string = '/qrApi/users/';
+  baseUrl: string = 'https://apiqr-production.up.railway.app/users/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getData(id: string): Promise<string> {
     return firstValueFrom(
-      this.http.get(this.baseUrl + id).pipe(
-        map(response => JSON.stringify(response))
-      )
+      this.http
+        .get(this.baseUrl + id)
+        .pipe(map((response) => JSON.stringify(response)))
     );
   }
 }
